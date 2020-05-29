aa60ec6081e1cbe8fa636cc2d214850771e51e72
Daniel <daniel@quintetsolutions.com>
2020-05-30 00:25:29 +0530

    Created Splash Screen activity

--- app/src/main/java/com/pebbles/ui/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/pebbles/ui/activities/SplashScreenActivity.kt
@@ -1,11 +1,12 @@
-package com.pebbles
+package com.pebbles.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pebbles.R
 
-class MainActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash_screen)
     }
 }
